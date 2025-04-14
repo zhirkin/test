34dc2df950d07fc10187cd7373047cb028777a6d
zhirkin <nikitka.zhirov@gmail.com>
2025-04-15 00:59:36 +0300

    feat

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,29 @@
-const gulp = require('gulp');
+const { src, dest, watch } = require('gulp');
 const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('browser-sync', function () {
- browserSync.init({
-  server: {
-   baseDir: "./"
-  }
- });
- gulp.watch("./*.html").on('change', browserSync.reload);
-});
+function bs() {
+    serveSass();
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    watch("./*.html").on('change', browserSync.reload);
+    watch("./sass/**/*.sass", serveSass);
+    watch("./js/*.js").on('change', browserSync.reload);
+};
+
+function serveSass() {
+    return src("./sass/*.sass")
+        .pipe(sass())
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 16 versions'],
+            cascade: false
+        }))
+        .pipe(dest("./css"))
+        .pipe(browserSync.stream());
+};
+
+exports.serve = bs
